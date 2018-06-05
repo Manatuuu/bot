3e031a31108e82bda99967d34ebaedf1be9d33e8
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2018-06-06 05:53:55 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,6 @@ client.on('ready', () => {
 client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.find('name', '参加-join');
     if (!channel) return;
-    console.log(member + 'がサーバーに参加しました (' + member.username + ')');
     channel.send(`ようこそ、${member}さん\nここはサーバーの知識を共有する場です\n質問以外の会話は ${member.guild.channels.find('name', 'chat-会話')} で行いましょう\n`);
     member.addRole(member.guild.roles.find('name','Member'));
 });
